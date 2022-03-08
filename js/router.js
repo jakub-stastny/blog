277ccd1dbc749d7a41a4e65c35ffcba82b7042ee
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-03-08 10:52:10 -0600

    Debug

--- js/router.js
+++ js/router.js
@@ -1,7 +1,7 @@
-//import { PostPage, PostIndex } from '/blog/js/post.js'
-import * as post from '/blog/js/post.js'
+import { PostPage, PostIndex } from '/blog/js/post.js'
+//import * as post from '/blog/js/post.js'
 
-export default post
+export default { PostPage, PostIndex }
 // routes = {
 //   "/": PostIndex,
 //   "/posts/:slug": PostPage,
