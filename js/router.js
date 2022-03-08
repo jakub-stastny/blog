1d5fed0b8299ae3ebc9079f6d689fc91289bf6ca
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-03-08 07:13:28 -0600

    Debug

--- js/router.js
+++ js/router.js
@@ -1,38 +1,38 @@
-import { PostPage, PostIndex } from './post.js'
+// import { PostPage, PostIndex } from './post.js'
 
-routes = {
-  "/": PostIndex,
-  "/posts/:slug": PostPage,
+// routes = {
+//   "/": PostIndex,
+//   "/posts/:slug": PostPage,
 
-  "/tags": null,
-  "/tags/:slug"
-}
+//   "/tags": null,
+//   "/tags/:slug"
+// }
 
-// [true, {slug: "hello-world"}]
-function matchChunks(routeChunk, locationChunk) {
-  if ((routeChunk.match(/^:(.+)$/) && locationChunk)) {
-    return {[RegExp.lastMatch]: locationChunk}
-  } else {
-    return routeChunk === locationChunk
-  }
-}
+// // [true, {slug: "hello-world"}]
+// function matchChunks(routeChunk, locationChunk) {
+//   if ((routeChunk.match(/^:(.+)$/) && locationChunk)) {
+//     return {[RegExp.lastMatch]: locationChunk}
+//   } else {
+//     return routeChunk === locationChunk
+//   }
+// }
 
-export default function router(locationChunks) {
-  const [ matchedRoute, matchedPageComponentClass ] = Object.entries(routes).find([ route, component ] => {
-    routeChunks = route.split('/').filter(chunk => chunk.length)
-    if (routeChunks.length === locationChunks.length) {
-      routeChunks.every((routeChunk, index) => matchChunks(routeChunk, locationChunks[index]))
-    }
-  })
+// export default function router(locationChunks) {
+//   const [ matchedRoute, matchedPageComponentClass ] = Object.entries(routes).find([ route, component ] => {
+//     routeChunks = route.split('/').filter(chunk => chunk.length)
+//     if (routeChunks.length === locationChunks.length) {
+//       routeChunks.every((routeChunk, index) => matchChunks(routeChunk, locationChunks[index]))
+//     }
+//   })
 
-  const params = matchedRoute.split('/').reduce((params, routeChunk) => {
-    let result = matchChunks(routeChunk, locationChunks[index])
-    if (typeof result === 'object') {
-      Object.assign({}, params, result)
-    } else {
-      params
-    }
-  }, {})
+//   const params = matchedRoute.split('/').reduce((params, routeChunk) => {
+//     let result = matchChunks(routeChunk, locationChunks[index])
+//     if (typeof result === 'object') {
+//       Object.assign({}, params, result)
+//     } else {
+//       params
+//     }
+//   }, {})
 
-  return new matchedPageComponentClass(params)
-}
+//   return new matchedPageComponentClass(params)
+// }
