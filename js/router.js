7469de5cdbeeb23bba224e37f62cf4ee81e85980
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-03-08 11:23:00 -0600

    Debug

--- js/router.js
+++ js/router.js
@@ -17,23 +17,22 @@ function matchChunks(routeChunk, locationChunk) {
   }
 }
 
-export default { x: routes }
-// export default function router(locationChunks) {
-//   const [ matchedRoute, matchedPageComponentClass ] = Object.entries(routes).find([ route, component ] => {
-//     routeChunks = route.split('/').filter(chunk => chunk.length)
-//     if (routeChunks.length === locationChunks.length) {
-//       routeChunks.every((routeChunk, index) => matchChunks(routeChunk, locationChunks[index]))
-//     }
-//   })
+export default function router(locationChunks) {
+  const [ matchedRoute, matchedPageComponentClass ] = Object.entries(routes).find([ route, component ] => {
+    routeChunks = route.split('/').filter(chunk => chunk.length)
+    if (routeChunks.length === locationChunks.length) {
+      routeChunks.every((routeChunk, index) => matchChunks(routeChunk, locationChunks[index]))
+    }
+  })
 
-//   const params = matchedRoute.split('/').reduce((params, routeChunk) => {
-//     let result = matchChunks(routeChunk, locationChunks[index])
-//     if (typeof result === 'object') {
-//       Object.assign({}, params, result)
-//     } else {
-//       params
-//     }
-//   }, {})
+  const params = matchedRoute.split('/').reduce((params, routeChunk) => {
+    let result = matchChunks(routeChunk, locationChunks[index])
+    if (typeof result === 'object') {
+      Object.assign({}, params, result)
+    } else {
+      params
+    }
+  }, {})
 
-//   return new matchedPageComponentClass(params)
-// }
+  return new matchedPageComponentClass(params)
+}
