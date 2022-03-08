b2bc0588163ec3821927531cac988bc168e6c78a
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-03-08 12:08:04 -0600

    Debug

--- js/router.js
+++ js/router.js
@@ -30,12 +30,13 @@ export default function router(locationChunks) {
   const [ matchedRoute, matchedPageComponentClass ] = result
 
   const params = matchedRoute.split('/').reduce((params, routeChunk) => {
-    let result = matchChunks(routeChunk, locationChunks[index])
-    if (typeof result === 'object') {
-      return Object.assign({}, params, result)
-    } else {
-      return params
-    }
+    return params
+    // let result = matchChunks(routeChunk, locationChunks[index])
+    // if (typeof result === 'object') {
+    //   return Object.assign({}, params, result)
+    // } else {
+    //   return params
+    // }
   }, {})
 
   return new matchedPageComponentClass(params)
